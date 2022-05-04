e8bce0a1590a7f111e914f2533c9c4e62f76f210
Muntaka Helali <helali.muntaka@gmail.com>
2022-05-04 03:27:42 -0400

    First called the viewModel Method with the amphibians api in the onCreateView method of the AmphibianListFragment.kt . Then implemented the listData and apiStatus binding from the viewModel in the fragment_amphibian_list.xml .

--- app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/AmphibianViewModel.kt
@@ -40,7 +40,7 @@ class AmphibianViewModel : ViewModel() {
     val amphibian : LiveData<Amphibian> = _amphibian
     // TODO: Create a function that gets a list of amphibians from the api service and sets the
     //  status via a Coroutine
-    private fun getAmphibianList(){
+    fun getAmphibianList(){
         viewModelScope.launch {
         _status.value = AmphibianApiStatus.LOADING
             try {
